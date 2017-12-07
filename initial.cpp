2e2d04f86efecdfa0e7cd312e1b9ed31c9d737c9
Fengkai Wan <fengkai@kth.se>
2017-12-07 11:21:42 +0100

    inital define

--- initial.cpp
+++ initial.cpp
@@ -1,6 +1,22 @@
-#include <math.h>
+# include <math.h>
+# include <stdio.h>
+# include <string.h>
+# include <cuda_runtime.h>
 
-#define 
+// universal gravitational constant in km
+# define G  6.67408E-20
+# define Epsilon 47.0975
+# define D 376.78
+# define Msi 7.4161E19   
+# define Mfe 1.9549E20
+# define Ksi 2.9114E14
+# define Kfe 5.8228E14
+# define KRPsi 0.01
+# define KRPfe 0.02
+# define SDPsi 0.001
+# define SDPfe 0.002
+# define Time_step 5.8117
+ 
 
 
 // interaction forces
